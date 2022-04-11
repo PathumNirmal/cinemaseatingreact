de9354afc789944ff4ea094afd83476923af007b
DeshanHarshana <nawarathnadeshan@gmail.com>
2022-04-11 04:09:16 +0000

    updated

--- server/api/api.js
+++ server/api/api.js
@@ -2,20 +2,19 @@ const express=require('express')
 const router=express.Router();
 const mongoose=require('mongoose');
 
-
+const db ="mongodb+srv://deshan:deshan2233@cluster0.4ynst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 //connect with database
-// mongoose.connect(db, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,}, err=>{
-//     if(err){
-//         console.log('Error With Connect MongoDB : ' + err)
-//     }
-//     else{
-//         console.log('Database Connect Successfully')
-//     }
-// })
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    }, err=>{
+    if(err){
+        console.log('Error With a Connect MongoDB : ' + err)
+    }
+    else{
+        console.log('Database Connect Successfully')
+    }
+})
 
 
 //routes
